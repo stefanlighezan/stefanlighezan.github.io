e6bb2214eb5d1eafd8c60227cdb58936896ffdda
stefanlighezan <sdlighe@gmail.com>
2024-07-05 20:52:09 -0700

    Firebase Config

---
+++ config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDchFtmBO2kAmHz5nXhJoynpkzYlOypTIU",
+    authDomain: "schoolviewapp-197d2.firebaseapp.com",
+    projectId: "schoolviewapp-197d2",
+    storageBucket: "schoolviewapp-197d2.appspot.com",
+    messagingSenderId: "74687319461",
+    appId: "1:74687319461:web:880eb0e5c043bf99896c4c",
+    measurementId: "G-DQHSNYT08D"
+};
+
+const config = {
+    apiKey: "AIzaSyDchFtmBO2kAmHz5nXhJoynpkzYlOypTIU",
+    authDomain: "schoolviewapp-197d2.firebaseapp.com",
+    projectId: "schoolviewapp-197d2",
+    storageBucket: "schoolviewapp-197d2.appspot.com",
+    messagingSenderId: "74687319461",
+    appId: "1:74687319461:web:880eb0e5c043bf99896c4c",
+    measurementId: "G-DQHSNYT08D"
+}
